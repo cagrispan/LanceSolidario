0d7efa857ebdb5332cf1736351b554740df1bb8b
Bruno Henrique Pereira Braga <brunofaczz@hotmail.com>
2016-08-12 18:02:30 -0300

    feat(users.middleware): create middleware for verify if the req has all information

--- backend/middlewares/users.middleware.js
+++ backend/middlewares/users.middleware.js
@@ -1,21 +1,19 @@
 'use strict';
 function UsersMiddleware() {
 
-  this.hasAllInformation = function(req, res, next) {
-    if(req.body){
-      if(req.body.card && req.body.name && req.body.role) {
-        next();
-      } else {
-        console.log(req.body.card + req.body.name + req.body.role);
-        res.send(404, {message: "parameters missing."})
-      }
-    } else {
-      console.log(req.body.card + req.body.name + req.body.role);
-      res.send(404, {message: "parameters missing."})
-    }
+    this.hasAllInformation = function (req, res, next) {
+        if (req.body) {
+            if (req.body.name && req.body.birthday && req.body.email && req.body.address && req.body.telephone) {
+                next();
+            } else {
+                res.send(404, {message: "parameters missing."})
+            }
+        } else {
+            res.send(404, {message: "parameters missing."})
+        }
 
 
-  };
+    };
 }
 UsersMiddleware.constructor = UsersMiddleware;
 module.exports = UsersMiddleware;
