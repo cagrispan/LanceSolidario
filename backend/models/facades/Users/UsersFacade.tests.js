20ab7e9b69ebb7623651227a1ab4cfd6c3ce140b
Bruno Henrique Pereira Braga <brunofaczz@hotmail.com>
2016-08-16 17:27:21 -0300

    test(UsersFacade.tests): create tests for usersFacade

--- backend/models/facades/Users/UsersFacade.tests.js
+++ backend/models/facades/Users/UsersFacade.tests.js
@@ -7,6 +7,11 @@ describe('UsersFacade Tests', function () {
 
     var usersFacade = new UsersFacade();
 
+    it('should be a function', function(done) {
+        expect(typeof usersFacade.createOrUpdate).to.equal('function');
+        done();
+    });
+
     it('should create or update an user on database', function (done) {
         usersFacade.facebookId = 'teste123456';
         usersFacade.email = 'teste@teste';
@@ -15,7 +20,7 @@ describe('UsersFacade Tests', function () {
             complement: "casa 1",
             street: "rua",
             city: "cidadezinha",
-            neighborhood: "vilaPinto",
+            neighborhood: "vila",
             state: "paranazao"
         };
         usersFacade.birthday = new Date('10/09/1998');
