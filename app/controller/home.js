1ad9e5950ffc3d8fb770c9ce0524a601d0bcbb37
Alisson Francisco krul <alisson.krul@hotmail.com>
2016-08-16 16:09:22 -0300

    feat(utils,home): add some dependecies

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,13 +1,15 @@
 'use strict';
 angular.module('lanceSolidario')
-.controller('HomeCtrl',  ['auth', '$location', function(auth, $location) {
+.controller('HomeCtrl',  ['auth', '$location','User', function(auth, $location, User) {
 
     var self = this;
 
     if(!auth.user){
         $location.path('/login');
+    }else{
+        var userToSave = new User();
+        userToSave
     }
-
     self.user = auth.user;
 
 }]);
--- app/services/utils.js
+++ app/services/utils.js
@@ -1,4 +1,4 @@
 /**
  * Created by Carlos on 23/07/2016.
  */
-angular.module('utils',[]);
+angular.module('utils',['lanceSolidario']);
