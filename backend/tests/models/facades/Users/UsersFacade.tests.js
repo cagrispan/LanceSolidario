06336c5451be8d2607c0bb2081eec40f512c9e90
Bruno Henrique Pereira Braga <brunofaczz@hotmail.com>
2016-08-24 16:57:42 -0300

    test(usersFacade.tests): create tests to 2 ways from function login

--- backend/tests/models/facades/Users/UsersFacade.tests.js
+++ backend/tests/models/facades/Users/UsersFacade.tests.js
@@ -2,17 +2,21 @@
 
 var expect = require('chai').expect;
 var UsersFacade = require('./../../../../src/models/facades/Users/UsersFacade');
+var q = require('q');
 
 describe('UsersFacade Tests', function () {
 
-    var usersFacade = new UsersFacade();
 
-    it('should be a function', function(done) {
+    it('should be a function', function (done) {
+        var usersFacade = new UsersFacade();
+
         expect(typeof usersFacade.createOrUpdate).to.equal('function');
         done();
     });
 
     it('should create or update an user on database', function (done) {
+        var usersFacade = new UsersFacade();
+
         usersFacade.facebookId = 'teste123456';
         usersFacade.email = 'teste@teste';
         usersFacade.address = {
@@ -28,12 +32,58 @@ describe('UsersFacade Tests', function () {
         usersFacade.facebookToken = 'tokenTest';
         usersFacade.token = 'hahaha';
 
-        usersFacade.createOrUpdate().then(function(result) {
+        usersFacade.createOrUpdate().then(function (result) {
             expect(result.dataValues.name).to.equal('testeZao');
             done();
-        }).catch(function(err) {
+        }).catch(function (err) {
             console.log(err);
         });
+
+    });
+
+    it('should update facebookToken if user already been created', function (done) {
+        var usersFacade = new UsersFacade();
+
+        usersFacade.facebookId = 'teste123456';
+        usersFacade.email = 'teste@teste';
+        usersFacade.birthday = new Date('10/09/1998');
+        usersFacade.name = 'testeZao';
+        usersFacade.facebookToken = 'tokenTestLogin';
+        usersFacade.token = 'lalala';
+
+        usersFacade.facebookLogin().then(function (result) {
+            console.log(result);
+            expect(result.dataValues.facebookToken).to.equal('tokenTestLogin');
+            expect(result.dataValues.token).to.equal('hahaha');
+            done();
+        });
+
+    });
+
+    it('should verify that does not have an user and create it', function (done) {
+        var usersFacade = new UsersFacade();
+
+        usersFacade.facebookId = makeid();
+        usersFacade.birthday = new Date('10/09/1998');
+        usersFacade.name = 'testeZao';
+        usersFacade.facebookToken = 'tokenTestLogin';
+
+        usersFacade.facebookLogin().then(function (result) {
+            console.log(result);
+            expect(result.dataValues.facebookToken).to.equal('tokenTestLogin');
+            done();
+        });
+
+        function makeid()
+        {
+            var text = "";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+            for( var i=0; i < 5; i++ )
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+            return text;
+        }
 
     });
 });
