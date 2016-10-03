7787e023468604cbb236a1b1f6c055953fb2ba7e
Lucas Ernesto Kindinger <kindingerlek@hotmail.com>
2016-10-03 10:12:24 -0300

    feat: update .less and html files

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -2,10 +2,10 @@
 angular.module('lanceSolidario')
     .controller('HomeCtrl', ['facebookAPI', '$location', function (facebookAPI, $location) {
         var self = this;
-        
+
         function init() {
             if (!facebookAPI.user) {
-                $location.path('/login');
+                $location.path('/home');
             } else {
                 var userToSave = facebookAPI.user;
                 self.user = userToSave;
